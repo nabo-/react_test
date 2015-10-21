c84a22855b24c8ac7e6ab81cbea339d1a8d80bf2
nabo- <kakugari50@gmail.com>
2015-10-22 01:00:08 +0900

    edit

--- src/js/form/module/UserList.jsx
+++ src/js/form/module/UserList.jsx
@@ -1,7 +1,25 @@
 var React = require('react');
 
 var UserList = React.createClass({
-
+	prpoTypes: {
+		userData: React.prpoType.arrayOf(React.prpoTypes.object).isRequired
+	},
+	render: function(){
+		var UserNodes = this.prpps.userData.map(function(user, index){
+			return (
+				<User name={user.name} mail={user.mail} key={index} />
+			);
+		});
+		return (
+			<table>
+			<tr>
+			<th>名前</th>
+			<th>メールアドレス</th>
+			</tr>
+			{UserNodes}
+			</table>
+		);
+	}
 });
 
 
